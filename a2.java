d1af8c3f87c0e6a40f12a534b63e7c13f9c01787
karthik <karthikreddym0000@gmail.com>
2026-03-23 19:00:15 +0530

    updated java files

---
+++ a2.java
@@ -0,0 +1,8 @@
+public class a2{
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming.");
+        System.out.println("System.out.println prints text on a new line....");
+    }
+}
